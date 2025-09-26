8a10e3f8d167db8612b36e30ba72c9dc5a0f7ee1
Xtremiz <fozanahmed01@gmail.com>
2025-09-26 21:06:36 +0500

    finally sorted the data according to churn's perspective + applied label encode to get numeric values out of rows!

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+
+import matplotlib.pyplot as plt
 
 le= LabelEncoder()
 df = pd.read_csv('s.csv')
 df.drop_duplicates(subset=['customerID'],inplace=True)
 
-print(df.info())
+for_churn = pd.get_dummies(df,columns=['Contract'], drop_first=False)
+
+for_churn = df[['SeniorCitizen', 'Partner','tenure','Dependents','InternetService','StreamingTV', 'StreamingMovies','Contract','PaymentMethod', 'MonthlyCharges','TotalCharges','Churn']]
+for_churn = pd.get_dummies(for_churn,columns=['InternetService','StreamingTV', 'StreamingMovies','Contract','PaymentMethod'], drop_first=False)
+for_churn.replace({True:1,False:0},inplace=True)
+print(for_churn.columns)
+
+"""for i in for_churn.columns:
+    print(f"--- {i} ------")
+    print(for_churn[i].value_counts())"""
